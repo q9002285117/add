5da86dcd8d09d8c92ee822a3dbdc112e48c7a683
q9002285117 <105021038@live.asia.edu.tw>
2016-09-17 16:18:38 +0800

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+package add;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8, b = 7, c;
+		c = a + b;
+		System.out.println(c);
+	}
+
+}
